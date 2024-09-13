a3e545334bbdaa123a457c0d9255a98c2af570c2
1makarov <38854356+1makarov@users.noreply.github.com>
2024-09-13 15:48:48 +0000

    macro only for SolEvent

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,11 @@ macro_rules! impl_sol_event_decode {
         use alloy_sol_types::SolEvent;
 
         $(
+            const _: fn() = || {
+                fn assert_impl<T: SolEvent>() {}
+                assert_impl::<$event_type>();
+            };
+            
             impl SolEventDecode for $event_type {
                 fn decode_log(log: &alloy_rpc_types::Log) -> alloy_sol_types::Result<Self> {
                     Self::decode_log_data(&log.inner, true)
