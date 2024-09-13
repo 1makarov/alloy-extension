025c73db3fac4b7a78ba8ddb9a891e51144374cb
1makarov <38854356+1makarov@users.noreply.github.com>
2024-09-13 15:37:14 +0000

    ))

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use alloy_rpc_types::Log;
 
 pub trait SolEventDecode: Sized {
-    fn encode_log(log: &Log) -> alloy_sol_types::Result<Self>;
+    fn decode_log(log: &Log) -> alloy_sol_types::Result<Self>;
 }
 
 #[macro_export]
 macro_rules! impl_sol_event_decode {
     ($event_type:ty) => {
         impl SolEventDecode for $event_type {
-            fn encode_log(log: &alloy_rpc_types::Log) -> alloy_sol_types::Result<Self> {
+            fn decode_log(log: &alloy_rpc_types::Log) -> alloy_sol_types::Result<Self> {
                 use alloy_sol_types::SolEvent;
                 Self::decode_log_data(&log.inner, true)
             }
@@ -17,7 +17,7 @@ macro_rules! impl_sol_event_decode {
     ($($event_type:ty),*) => {
         $(
             impl SolEventDecode for $event_type {
-                fn encode_log(log: &alloy_rpc_types::Log) -> alloy_sol_types::Result<Self> {
+                fn decode_log(log: &alloy_rpc_types::Log) -> alloy_sol_types::Result<Self> {
                     use alloy_sol_types::SolEvent;
                     Self::decode_log_data(&log.inner, true)
                 }
