6262e02ce7e7fab42722921b33be5a538bbce99b
1makarov <38854356+1makarov@users.noreply.github.com>
2024-09-13 15:36:18 +0000

    init

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use alloy_rpc_types::Log;
+
+pub trait SolEventDecode: Sized {
+    fn encode_log(log: &Log) -> alloy_sol_types::Result<Self>;
+}
+
+#[macro_export]
+macro_rules! impl_sol_event_decode {
+    ($event_type:ty) => {
+        impl SolEventDecode for $event_type {
+            fn encode_log(log: &alloy_rpc_types::Log) -> alloy_sol_types::Result<Self> {
+                use alloy_sol_types::SolEvent;
+                Self::decode_log_data(&log.inner, true)
+            }
+        }
+    };
+    ($($event_type:ty),*) => {
+        $(
+            impl SolEventDecode for $event_type {
+                fn encode_log(log: &alloy_rpc_types::Log) -> alloy_sol_types::Result<Self> {
+                    use alloy_sol_types::SolEvent;
+                    Self::decode_log_data(&log.inner, true)
+                }
+            }
+        )*
+    };
+}
