2d89c24de09707453da2fb123fb014480caf5036
1makarov <38854356+1makarov@users.noreply.github.com>
2024-09-13 15:42:35 +0000

    upgrade

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,16 @@ pub trait SolEventDecode: Sized {
 
 #[macro_export]
 macro_rules! impl_sol_event_decode {
-    ($event_type:ty) => {
-        impl SolEventDecode for $event_type {
-            fn decode_log(log: &alloy_rpc_types::Log) -> alloy_sol_types::Result<Self> {
-                use alloy_sol_types::SolEvent;
-                Self::decode_log_data(&log.inner, true)
-            }
-        }
-    };
-    ($($event_type:ty),*) => {
+    ($($event_type:ty),+) => {
+        use alloy_extension::SolEventDecode;
+        use alloy_sol_types::SolEvent;
+
         $(
             impl SolEventDecode for $event_type {
                 fn decode_log(log: &alloy_rpc_types::Log) -> alloy_sol_types::Result<Self> {
-                    use alloy_sol_types::SolEvent;
                     Self::decode_log_data(&log.inner, true)
                 }
             }
-        )*
+        )+
     };
 }
